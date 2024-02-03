ed868c4b39dd9da6d3ff27fb3a3a50bcd076d46d
Raphael Salazar <127420583+pngRMS@users.noreply.github.com>
2024-02-03 16:33:08 +0800

    Cleaning code structure

--- src/Component/cardData.js
+++ src/Component/cardData.js
@@ -2,11 +2,9 @@ const cardData = [
   {
     name: "New",
     description: "Upload",
-    price: 6,
     photoName: "./images/newfile.png",
     upload: "./images/upload.png",
   },
 ];
-// abc
 
 export default cardData;
